61a7310f78108fa968947751ad7587521c85f8d9
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2023-07-25 09:46:13 +0700

    230725 - BLTAnh - Fix Router

--- services/src/index.tsx
+++ services/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './tests/reportWebVitals';
 
 import Game from './pages/game';
@@ -9,13 +9,13 @@ import Home from './pages/home';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/games" element={<Game />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
