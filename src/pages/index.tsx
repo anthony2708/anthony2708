e6ab757bf72dad1f8e16c7c87a2eb44686a6af51
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2024-05-24 16:25:31 +0700

    240524 - BLTAnh - 90days updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -80,8 +80,8 @@ function HomepageHeader() {
                 <div className={styles.buttons}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/docs/intro">
-                        <Translate id='homepage.button'>Giới thiệu - 5 phút ⏱️</Translate>
+                        href="https://90days.builetuananh.name.vn">
+                        <Translate id='homepage.button'>90 ngày DevOps ♾</Translate>
                     </Link>
                 </div>
             </div>
