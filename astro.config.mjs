914c5a1fae333551a396814c3ed76ddba7fd659e
Anthony Bui <builetuananh2708@gmail.com>
2024-11-12 21:20:55 +0700

    241112 - BLTAnh - Add new portfolio

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://www.builetuananh.name.vn',
+  // base: 'anthony2708',
+});
