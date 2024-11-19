2cad98d1060862420ad9cc58a9658c196f4e5902
Anthony Bui <builetuananh2708@gmail.com>
2024-11-19 19:54:09 +0700

    241119 - BLTAnh - Merge 90days with this, move to new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.builetuananh.name.vn',
   // base: 'anthony2708',
+  site: 'https://www.builetuananh.name.vn',
+
+  integrations: [starlight({
+    title: "BLTA",
+    disable404Route: true,
+  })],
 });
---
+++ src/content/docs/90days/index.md
@@ -0,0 +1,5 @@
+---
+title: 90 ngày cùng DevOps
+---
+
+Trang đang trong quá trình xây dựng, vui lòng quay lại sau.
